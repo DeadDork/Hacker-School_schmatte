4a7bb215219bc857dbba0b10a0c5cbd95688c1f5
Nimrod Omer <nimrodomer@gmail.com>
2012-10-07 03:32:53 -0400

    Horizontal histogram of word counts.

---
+++ c/ansi_c/ch_1/exercise_1-13.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main() {
+	int c, i, j, nc, state;
+	int word_length[81];
+
+	state = OUT;
+	nc = 0;
+	for (i = 0; i < 81; ++i) {
+		word_length[i] = 0;
+	}
+
+	while ((c = getchar()) != EOF) {
+		++nc;
+		if (c == ' ' || c == '\n' || c == '\r' || c == '\t') {
+			if (state == IN) {
+				if (nc < 81) {
+					++word_length[nc - 1]; // For words no longer than 80 chars.
+				} else {
+					++word_length[80]; // For words longer than 80 chars.
+				}
+			}
+			state = OUT;
+			nc = 0;
+		} else {
+			if (state == OUT) {
+				state = IN;
+			}
+		}
+	}
+
+	for (i = 0; i < 81; ++i) {
+		printf("Word length = %-10d: ", i);
+		for (j = 0; j <= word_length[i]; ++j) {
+			printf("|");
+		}
+		printf("\n");
+	}
+	printf("Word length > 80        : ");
+	for (j = 0; j <= word_length[80]; ++j) {
+		printf("|");
+	}
+	printf("\n");
+
+	return 0;
+}
