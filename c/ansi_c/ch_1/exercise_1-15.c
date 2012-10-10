aa2d3c75c433a91120b2ae27902c2fb8c2d7fc8a
Nimrod Omer <nimrodomer@gmail.com>
2012-10-10 18:44:43 -0400

    Simplified & renamed function.

--- c/ansi_c/ch_1/exercise_1-15.c
+++ c/ansi_c/ch_1/exercise_1-15.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 // Function prototype.
-float convert(int m);
+float fahr2cels(int m);
 
 // Prints out the celsius values from 0 Fahrenheit - 300 Fahrenheit.
 int main() {
 	int fahr;
 
 	for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
-		printf("%3d %6.1f\n", fahr, convert(fahr));
+		printf("%3d %6.1f\n", fahr, fahr2cels(fahr));
 	}
 
 	return 0;
 }
 
 // Uses the conversion: Celsius = (5/9) * Fahrenheit - 32.
-float convert(int fahr) {
-	float cels = ((5.0 / 9.0) * fahr - 32);
-
-	return cels;
+float fahr2cels(int fahr) {
+	return ((5.0 / 9.0) * fahr - 32);
 }
