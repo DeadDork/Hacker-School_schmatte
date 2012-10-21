05d6c80a0d1a0ed183e04aab0504ce743be994a7
Nimrod Omer <nimrodomer@gmail.com>
2012-10-21 02:30:03 -0400

    Fixed stack-smash bug.

--- c/ansi_c/ch_1/exercise_1-18.c
+++ c/ansi_c/ch_1/exercise_1-18.c
@@ -1,43 +1,58 @@
 #include <stdio.h>
-
-#define MAXIMUM 1000 // Maximum characters per line.
+// Maximum characters per line.
+#define MAXIMUM 1000
 
 // Ranges of ASCII values for charstream.
-#define SPACE ' ' // ASCII value for space (32).
-#define TILDE '~' // ASCII value for ~ (126).
+#define CHARSET_START '!' // ASCII value for space (32).
+#define CHARSET_END '~' // ASCII value for ~ (126).
 
 int main() {
-	int c, i, end;
-	char line[MAXIMUM];
 
-	int element = 0;
-	int empty = 1; // 0 == FALSE & 1 == TRUE.
+	int c, i;
+	char  line[MAXIMUM];
+
+	int index = 0;
+	int l_end = 1; // 0 == FALSE & 1 == TRUE.
 
 	while ((c = getchar()) != EOF) {
-		if (element < MAXIMUM) {
-			if (c != '\n') {
-				line[element] = c;
-			} else {
-				for (i = 0; i <= element; ++i) {
-					if (line[i] >= SPACE && line[i] <= TILDE) {
-						empty = 0;
-					}
-					if (line[element] > SPACE && line[element] <= TILDE) {
-						end = i;
-					}
-				}
-				if (empty == 0) {
-					for (i = 0; i <= end; ++i) {
-						printf("%c", line[i]);
-					}
-				}
-				printf("\n");
-				element = 0;
-				empty = 1;
+
+		// if the line is at an end or exceeds the maximum length of the line.
+		if (l_end == 0 && (c == '\n' || index >= MAXIMUM)) {
+
+			// Use a for loop to get rid of any trailing white space.
+			for (index; line[index] < CHARSET_START && line[index] > CHARSET_END; --index) {
+				;
 			}
-			++element;
+
+			// Print the line.
+			for (i = 0; i < index; ++i) {
+				printf("%c", line[i]);
+			}
+			printf("\n");
+
+			// Line end is set to TRUE.
+			l_end = 1;
+
 		}
+
+		// Resets the character counter for that line.
+		if (c == '\n') {
+			index = 0;
+		}
+
+		// Sets the line end to false when the first character in the charstream after a newline is not a newline (e.g empty row).
+		if (index == 0 && c != '\n') {
+			l_end = 0;
+		}
+
+		// Assigns characters to the string array.
+		if (l_end == 0) {
+			line[index] = c;
+			++index;
+		}
+
 	}
 
 	return 0;
+
 }
